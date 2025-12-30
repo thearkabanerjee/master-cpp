e38e77ef037226837068b214db4053c98a34412e
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-30 11:04:26 +0530

    mastering c++

--- primitives.cpp
+++ primitives.cpp
@@ -1,2 +1,14 @@
 //datatypes 
 
+#include <iostream>
+using namespace std;
+
+int main (){
+    int number = 92;
+    const string name = "Arka Banerjee";
+
+    char grade = 'A';
+
+    cout << "your name is " << name << " and your grade is " << grade  << endl;
+    return 0;
+}
