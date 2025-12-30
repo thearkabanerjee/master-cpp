b46dfe2e62b9fa81fd8bad2fdfe92479b67dc533
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-30 10:47:49 +0530

    mastering c++

---
+++ index.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main (){
+
+    string hitesh = "Hitesh Choudhary"; // this is normal thingy of c++ (variable)
+    const int hello = 1232133124; // this is the const thingy of c++ (constant)
+    
+    cout << "Hello world" << endl;
+    cout << "Welcome to chai with cpp " << hello <<endl;
+    return 0;
+}
