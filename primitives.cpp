963f6a9552b920f1afcc6969c59e9f569ab3aed2
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-30 11:09:12 +0530

    mastering c++

--- primitives.cpp
+++ primitives.cpp
@@ -8,7 +8,15 @@ int main (){
     const string name = "Arka Banerjee";
 
     char grade = 'A';
+    bool passed = true;
 
-    cout << "your name is " << name << " and your grade is " << grade  << endl;
+    string lastpart;
+    if (passed == true) {
+        lastpart = "He has passed";
+    }
+    else {
+        lastpart ="He has not passed";
+    }
+    cout << "your name is " << name << " and your grade is " << grade  <<'.'<< lastpart<< endl;
     return 0;
 }
