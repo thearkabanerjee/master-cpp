a4e22eaa5a520e253544857e72fb9e4797ea01af
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2025-12-30 11:33:51 +0530

    mastering c++

---
+++ test.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    // int number1, number2;
+    // cout << "number 1 = ";
+    // cin >> number1;
+
+    // cout << "number 2 = ";
+    // cin >> number2;
+    // int sum = (number1 +number2);
+
+    // cout << "sum of 2 numbers= "<< sum << endl;
+
+    int number ;
+    cout << "Enter the number ";
+    cin >> number;
+
+    string number2 = to_string(number);
+    
+
+    cout << number << endl;
+
+    return 0;
+}
